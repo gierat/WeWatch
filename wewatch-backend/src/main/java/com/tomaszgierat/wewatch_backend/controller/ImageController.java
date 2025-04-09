81f159907bb0e099b8c7a01c6260448ac1cf27c4
Tomasz <gieratdoc@gmail.com>
2025-04-09 22:38:39 +0200

    feat(api): add image controller & fix images routing

---
+++ wewatch-backend/src/main/java/com/tomaszgierat/wewatch_backend/controller/ImageController.java
@@ -0,0 +1,30 @@
+package com.tomaszgierat.wewatch_backend.controller;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.io.IOException;
+import java.nio.file.*;
+
+@RestController
+@RequestMapping("/api/images")
+@CrossOrigin("*")
+public class ImageController {
+
+    @GetMapping("/movies/{filename:.+}")
+    public ResponseEntity<Resource> getMovieImage(@PathVariable String filename) throws IOException {
+        Path filePath = Paths.get("uploads/movies").resolve(filename).normalize();
+        Resource file = new UrlResource(filePath.toUri());
+
+        if (!file.exists() || !file.isReadable()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, Files.probeContentType(filePath))
+                .body(file);
+    }
+
+}
