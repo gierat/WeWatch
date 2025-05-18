eef84b5103084316ce234c2a8515ab99059b7ff5
Tomasz <gieratdoc@gmail.com>
2025-05-18 23:50:27 +0200

    feat(comments): add basic CommentController

---
+++ wewatch-backend/src/main/java/com/tomaszgierat/wewatch_backend/controller/CommentController.java
@@ -0,0 +1,24 @@
+package com.tomaszgierat.wewatch_backend.controller;
+
+import com.tomaszgierat.wewatch_backend.dto.request.CommentRequest;
+import com.tomaszgierat.wewatch_backend.dto.response.CommentResponse;
+import com.tomaszgierat.wewatch_backend.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/comments")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @GetMapping("/movie/{movieId}")
+    public ResponseEntity<List<CommentResponse>> getComments(@PathVariable Long movieId) {
+        return ResponseEntity.ok(commentService.getCommentsForMovie(movieId));
+    }
+}
