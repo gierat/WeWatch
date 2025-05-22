6c9f2de71c03c4a9f97c20c5a1c81623be6ac897
Tomasz <gieratdoc@gmail.com>
2025-05-22 10:04:09 +0200

    feat(RabbitMQ): add RabbitMQ config with comments.queue

---
+++ wewatch-backend/src/main/java/com/tomaszgierat/wewatch_backend/rabbit/RabbitMQConfig.java
@@ -0,0 +1,31 @@
+package com.tomaszgierat.wewatch_backend.rabbit;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String COMMENT_QUEUE = "comments.queue";
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jackson2JsonMessageConverter());
+        return template;
+    }
+    @Bean
+    public Queue commentQueue() {
+        return new Queue(COMMENT_QUEUE, true);
+    }
+
+}
