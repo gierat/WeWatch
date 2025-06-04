cebc1072fd0b93552c16ca540bfe10085f469280
Tomasz <gieratdoc@gmail.com>
2025-06-04 23:18:26 +0200

    docs(MovieController): add Swagger documentation for movie endpoints

--- wewatch-backend/src/main/java/com/tomaszgierat/wewatch_backend/controller/MovieController.java
+++ wewatch-backend/src/main/java/com/tomaszgierat/wewatch_backend/controller/MovieController.java
@@ -3,6 +3,7 @@ package com.tomaszgierat.wewatch_backend.controller;
 import com.tomaszgierat.wewatch_backend.dto.response.MovieResponse;
 import com.tomaszgierat.wewatch_backend.service.MovieService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
@@ -24,14 +25,22 @@ public class MovieController {
             @ApiResponse(responseCode = "200", description = "Successfully returned comments"),
             @ApiResponse(responseCode = "404", description = "Movie not found"),
     })
-
     @GetMapping("/{id}")
-    public ResponseEntity<?> getMovieById(@PathVariable Long id) {
+    public ResponseEntity<?> getMovieById(
+            @Parameter(description = "The ID of the movie to retrieve", example = "1")
+            @PathVariable Long id) {
         return movieService.getMovieById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(
+            summary = "Get all movies",
+            description = "Returns a list of movies (limited to the first 8 in ascending order by ID)"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully returned list of movies")
+    })
     @GetMapping
     public List<MovieResponse> getAllMovies() {
         return movieService.getAllMovies();
